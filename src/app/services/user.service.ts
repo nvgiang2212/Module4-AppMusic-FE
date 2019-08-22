589db8958cce1ca1c9c6c03f5f7705420e639a3c
vgiang <Giang221295>
2019-08-23 01:57:27 +0700

    create user services

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {config} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getAll() {
+    return this.http.get<any[]>(`${config.Promise}/users`);
+  }
+
+  register(user) {
+    return this.http.post(`${config.Promise}/users/register`, user);
+  }
+
+  delete(id) {
+    return this.http.delete(`${config.Promise}/users/${id}`);
+  }
+
 }
