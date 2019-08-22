11bd955a070dfed6626eb16e12d963d70aa36be7
vgiang <Giang221295>
2019-08-23 02:10:49 +0700

    update alert component

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {AlertService} from '../services/alert.service';
 
 @Component({
   selector: 'app-alert',
@@ -7,13 +9,29 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 })
 export class AlertComponent implements OnInit, OnDestroy {
 
-  constructor() {
-  }
+  private subscription: Subscription;
+  message: any;
+
+  constructor(private alertService: AlertService) { }
 
   ngOnInit() {
+    this.subscription = this.alertService.getAlert()
+      .subscribe(message => {
+        switch (message && message.type) {
+          case 'success':
+            message.cssClass = 'alert alert-success';
+            break;
+          case 'error':
+            message.cssClass = 'alert alert-danger';
+            break;
+        }
+
+        this.message = message;
+      });
   }
 
-  ngOnDestroy(): void {
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
